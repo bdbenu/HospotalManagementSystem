a0f98e6f31e6aac075c5ed60b57f97a7c9c91198
bdbenu <bdbenu18@gmail.com>
2024-02-18 08:49:44 +0530

    This is the initial commit

---
+++ HospitalManagementSyste/src/test/java/com/hospital/HospitalManagementSysteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSysteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
